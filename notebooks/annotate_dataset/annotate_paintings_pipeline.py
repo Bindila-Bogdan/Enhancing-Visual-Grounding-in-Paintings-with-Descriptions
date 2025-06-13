f5deeb3940cd2162e7e632622b9eb11913bf2aa4
Bindila-Bogdan <bogdan@meshh.com>
2025-06-13 14:38:03 +0200

    prepare pipeline for running

--- notebooks/annotate_dataset/annotate_paintings_pipeline.py
+++ notebooks/annotate_dataset/annotate_paintings_pipeline.py
@@ -246,7 +246,7 @@ def annotate_paintings_batch(start_index, stop_index, store_freq=25):
         ]
 
         # ground objects
-        labels_scores_boxes, _ = detect_objects(
+        labels_scores_boxes = detect_objects(
             image,
             predicted_objects,
             grounding_processor,
@@ -269,13 +269,15 @@ def annotate_paintings_batch(start_index, stop_index, store_freq=25):
                 objects_descriptions[index],
             ]
 
+        extracted_objects = list(predicted_spans_per_object.keys())
+
         paintings_annotations.append(
             {
                 "painting_id": painting["painting_id"],
                 "objects": predicted_spans_per_object,
                 "bounding_boxes": [
                     [bbox[0], float(bbox[1]), [float(bbox_coord) for bbox_coord in bbox[2]]]
-                    for bbox in labels_scores_boxes
+                    for bbox in labels_scores_boxes if bbox[0] in extracted_objects
                 ],
                 "extraction_judgement": {**extraction_judgement, **extraction_judge_metrics},
                 "description_judgement": description_judgement,
